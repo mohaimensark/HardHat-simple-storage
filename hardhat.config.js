851f473115f575351ca1b320a582affc55311d30
mohaimensark <mohaimensarkerofficial@gmail.com>
2023-02-21 23:51:55 +0600

    hi

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+//require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("./tasks/block-number")
+require("@nomiclabs/hardhat-etherscan")
+require("solidity-coverage")
+require("dotenv").config()
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 module.exports = {
+  defaultNetwork:"hardhat",
+  neworks:{
+    goerli:{
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId:5,
+
+    },
+   
+  },
   solidity: "0.8.17",
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,28 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+//imports
+const {ethers} = require("hardhat");
+require("dotenv").config()
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+// async main
+async function main(){
+  const SimpleStorageFactory = await ethers.getContractFactory(
+    "SimpleStorage"
+  )
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Deploying contract ...");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.deployed();
+  console.log(`Deployed contract to : ${simpleStorage.address}`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
+
+async function verify(contractAddress, args){
+    
+}
+
+
+// main{}
+main().then(()=> process.exit(0))
+.catch((error)=>{
   console.error(error);
-  process.exitCode = 1;
+  process.exit(1);
 });
